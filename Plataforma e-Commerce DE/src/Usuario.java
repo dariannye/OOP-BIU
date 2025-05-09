0ee3de594a747fe0b649227ddd422e7068351742
dariannye <dariannyn19@gmail.com>
2025-05-08 20:55:10 -0400

    Clase usuario

---
+++ Plataforma e-Commerce DE/src/Usuario.java
@@ -0,0 +1,48 @@
+
+public class Usuario {
+    private String nombre, correoElectronico, contrasena;
+
+    public Usuario(String nombre, String correoElectronico, String contrasena) {
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+    }
+    
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+    
+    public String getContrasena() {
+        return contrasena;
+    }
+
+  
+    
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void registrar() {
+        System.out.println("Usuario registrado: " + nombre);
+    }
+
+    public void iniciarSesion() {
+        System.out.println("Inicio de sesión: " + correoElectronico);
+    }
+}
