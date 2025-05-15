41603f4d70694b3ea6ce3454f2ecdccaf9eb9b9d
dariannye <dariannyn19@gmail.com>
2025-05-15 18:40:46 -0400

    Clase producto fisico

---
+++ Plataforma e-Commerce DE/src/ProductoFisico.java
@@ -0,0 +1,31 @@
+
+public class ProductoFisico extends Producto {
+	private double pesoLib;
+	private String dimensiones;
+
+	public ProductoFisico(String id, String nombre, double precio, int idInventario, double pesoLib, String dimensiones) {
+		super(id, nombre, precio, idInventario);
+		this.pesoLib = pesoLib;
+        this.dimensiones = dimensiones;
+	}
+	
+	//Getters
+    public double getPesoLib() { 
+    	return pesoLib; 
+    }
+    
+    public String getDimensiones() { 
+    	return dimensiones; 
+    }
+    
+   
+    //Setters
+    public void setPesoLib(double pesoLib) { 
+    	this.pesoLib = pesoLib; 
+    }
+    
+    public void setDimensiones(String dimensiones) {
+    	this.dimensiones = dimensiones; 
+    }
+
+}
